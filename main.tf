d0f397d5d63c740d678c79593fd06b2341fe32f9
FaisalWaheed677 <126425478+FaisalWaheed677@users.noreply.github.com>
2024-10-18 16:23:03 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+}
+
+module "app_service" {
+  source                = "./tf-app-service"
+  resource_group_name   = "FaisalsRSG"
+  location              = "East US"
+  app_service_plan_name = "FaisalsASP"
+  app_service_name      = "FaisalsAS"
+}
+
